66b6517d8a1b3a9641cbf26cd698fdf60bae1356
YSilahin <egor.silagin96@gmail.com>
2021-06-16 11:21:36 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //input
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a number : ");
         String str = sc.nextLine();
