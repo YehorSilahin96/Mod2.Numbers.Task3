bfc5c6b7a96c0d48249a3d47fbd896ba6d89d310
YSilahin <egor.silagin96@gmail.com>
2021-06-23 12:46:01 +0300

    Renamed variables, fixed structure of code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,31 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        //input
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a number : ");
-        String str = sc.nextLine();
+        String inputNumber = sc.nextLine();
 
-        int length = str.length();
+        int length = inputNumber.length();
         int counter = 0;
-        char chr;
+        char digit;
 
-        for(int i=1;i<length;i++)
-        {
-            chr = str.charAt(i);
-            if(chr=='0')
+        for (int i = 1; i < length; i++) {
+            digit = inputNumber.charAt(i);
+            if (digit == '0') {
                 counter++;
+            }
         }
 
-        char first = str.charAt(0);
+        char first = inputNumber.charAt(0);
 
-        if(counter>0 && first !='0')
-            System.out.println("Duck number");
-
-        else
+        if (counter > 0 && first != '0') {
+            System.out.println("It's a duck number");
+        }
+        else {
             System.out.println("Not a duck number");
-
+        }
     }
 }
